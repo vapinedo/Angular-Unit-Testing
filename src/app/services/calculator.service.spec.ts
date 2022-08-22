840395e4852484bcdb74f21fb7072bfc2dd58787
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-08-22 15:39:49 -0500

    [add] Testing the Service which has another service injected through Dependency Injection

--- src/app/services/calculator.service.spec.ts
+++ src/app/services/calculator.service.spec.ts
@@ -1,14 +1,17 @@
 import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
 
 describe('CalculatorService', () => {
   it('should add two numbers', () => {
-    const calculator = new CalculatorService();
+    const loggerSvc = new LoggerService();
+    const calculator = new CalculatorService(loggerSvc);
     let result = calculator.add(2, 3);
     expect(result).toBe(5);
   });
 
   it('should subtract two numbers', () => {
-    const calculator = new CalculatorService();
+    const loggerSvc = new LoggerService();
+    const calculator = new CalculatorService(loggerSvc);
     let result = calculator.subtract(4, 2);
     expect(result).toBe(2);
   });
--- src/app/services/calculator.service.ts
+++ src/app/services/calculator.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
+import { LoggerService } from './logger.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CalculatorService {
+
+  constructor(private loggerSvc: LoggerService) {}
+  
   add(num1: number, num2: number): number {
+    this.loggerSvc.log("Add operation has been called"); 
     return num1 + num2;
   }
-
+  
   subtract(num1: number, num2: number): number {
-    return num1 - num2;
+    this.loggerSvc.log("Subtract operation has been called"); 
+    return num1 -   num2;
   }
 }
