3b5cc6c3dcd3032b39713a8ef64be01c9f2671e6
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-08-28 12:21:27 -0500

    [add] Create Component instance using Angular TestBed with configureTestingModule

--- src/app/components/post/post.component.spec.ts
+++ src/app/components/post/post.component.spec.ts
@@ -1,10 +1,25 @@
 import { first } from "rxjs";
-import { PostI } from "src/app/interfaces/post.interface";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { PostComponent } from "./post.component";
+import { PostI } from "src/app/interfaces/post.interface";
 
 describe("Post Componnent", () => {
-    it("shoudl raise an event when the delete post is clicked",  () => {
-        const component = new PostComponent();
+    let fixture: ComponentFixture<PostComponent>;
+    let component: PostComponent;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [PostComponent],
+        });
+        fixture = TestBed.createComponent(PostComponent);
+        component = fixture.componentInstance;
+    });
+
+    it("should create post component using TestBed", () => {
+        expect(component).toBeDefined();
+    });
+
+    it("should raise an event when the delete post is clicked",  () => {
         const post: PostI = { id: 1, body: "body 1", title: "title 1" };
         component.post = post;
         component.delete.pipe(first()).subscribe(selectedPost => {
